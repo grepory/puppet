e65d852e209ac0bbe84aac1353020683358c56e7
Greg Poirier <greg.poirier@opower.com>
2013-10-10 07:21:12 -0700

    add comments to Puppet::Parser::Opower

--- lib/puppet/parser/opower.rb
+++ lib/puppet/parser/opower.rb
@@ -1,3 +1,14 @@
+# Puppet::Parser::Opower
+#
+# Author: Greg Poirier <greg.poirier@opower.com>
+#
+# Singleton two-layer cache machanism intended for use with a modified version
+# of extlookup.
+#
+# Attributes:
+# extlookup - Top-level cache used by extlookup
+# lookup    - Second-level cache used by CSV and YAML search functions
+
 module Puppet::Parser
   class Opower
 
@@ -10,13 +21,23 @@ module Puppet::Parser
       end
     end
 
+    # cache
+    #
+    # Parameters:
+    # key: cache you wish to retrieve
+    #
+    # E.g. cache = Puppet::Parser::Opower.cache('hostname.va.opower.it')
     def self.cache(key)
       @@cache ||= {}
       @@cache[key] ||= Cache.new
     end
 
+    # delete
+    #
+    # Parameters:
+    # key: cache you wish to delete
     def self.delete(key)
-      @@cache.delete(key) if @@cache
+      @@cache.delete(key)
     end
 
   end
